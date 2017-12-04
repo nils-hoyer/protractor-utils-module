63006090be2954ca82eff492bf361f7952547b6a
nhoyer <Nils.Hoyer@interone.de>
2017-12-04 10:34:18 +0100

    refactored to Utils class, minor function adjustments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # protractor-utils-module
-adds & extends functions with implicit wait
+Extends basic Protractor functionality to ensures visibility of elements before interaction. Added useful custom functions to the utils.
 
 # usage
 The protractor-utils-module is available via npm:
@@ -12,8 +12,52 @@ In your protractor configuration file, register the module:
 const protractorUtilsModule = require('protractor-utils-module');
 
 exports.config = {
-   onPrepare: function() {
-      browser.utils = protractorUtilsModule;
-   }
+    baseUrl: 'http://www.base.url',
+    
+    onPrepare: function() {
+    
+        // initialization
+        browser.utils = new ProtractorUtilsModule();
+        
+        // custom initialization   
+        const config = {
+            wait: 200,
+            waitClickable: 5000,
+            scrollIntoView: false,
+        }
+        const configImageComparison = {
+            baselineFolder: 'path/to/baselineFolder'
+            screenshotPath: 'path/to/screenshotPath'
+        }
+        browser.utils = new ProtractorUtilsModule(config, configImageComparison);
+    }
+   
 }
 ```
+
+in your jasmine test you can use the following functions:
+```
+const ele = element(by.css('.element'));
+const opt = element(by.css('option'));
+
+describe('protractor-utils-module', function() {
+    it('example usage', function() { 
+        browser.utils.get('/path');
+        browser.utils.click(ele);
+        browser.utils.click(ele, {wait: 1000});
+        browser.utils.clickDropdown(ele, opt, 3);
+        browser.utils.sendKeys(ele, 'text');
+        browser.utils.sendKeys(ele, 'text', {scrollIntoView: true});
+        browser.utils.getText(ele);
+        browser.utils.isDisplayed(ele);
+        browser.utils.hasClass(ele, 'has-error');
+        browser.utils.clearBrowserInstance();
+        browser.utils.checkScreen('name');
+        browser.utils.checkElement('name', ele);
+        browser.utils.checkElement('name', ele, {waitClickable: 15000});
+        browser.utils.switchToTab(1);      
+    });
+});
+```
+
+
--- Utils.js
+++ Utils.js
@@ -1,207 +1,214 @@
+'use strict';
+
 const protractorImageComparison = require('protractor-image-comparison');
 
 /**
- * extended selenium element.click function
- * @param element
- * @param options
+ * Protractor Utils Helper Class
  */
-function click(element, options) {
-    options = optionSetter(options);
-    optionExecutor(element, options);
-    return element.click();
-}
+class ProtractorUtilsModule {
 
-/**
- * function to select an element of a dropdown list
- * @param element
- * @param elementOption
- * @param elementOptionNumber
- * @param options
- */
-function clickDropdown(element, elementOption, elementOptionNumber, options) {
-    options = optionSetter(options);
-    click(element, options);
-    return click(element.all(elementOption).get(elementOptionNumber), options);
-}
+    constructor(config, configImageComparison) {
 
-/**
- * extended selenium element.sendKeys function
- * @param element
- * @param text
- * @param options
- */
-function sendKeys(element, text, options) {
-    options = optionSetter(options);
-    optionExecutor(element, options);
-    return element.clear().then(function() {
-        return element.sendKeys(text);
-    });
-}
+        this.config = {
+            wait: config && config.hasOwnProperty('wait') ? config.wait : 500,
+            waitClickable: config && config.hasOwnProperty('waitClickable') ? config.waitClickable : 10000,
+            scrollIntoView: config && config.hasOwnProperty('scrollIntoView') ? config.scrollIntoView : false,
+            imageComparison: config && config.hasOwnProperty('imageComparison') ? config.imageComparison : {},
+        }
 
-/**
- * extended selenium element.getText function
- * @param element
- * @param options
- * @returns {webdriver.promise.Promise.<string>|promise.Promise<string>}
- */
-function getText(element, options) {
-    options = optionSetter(options);
-    optionExecutor(element, options);
-    return element.getText();
-}
+        this.protractorImageComparison = new protractorImageComparison({
+            baselineFolder: configImageComparison && configImageComparison.hasOwnProperty('baselineFolder') ?
+                configImageComparison.baselineFolder : './images/expected/',
+            screenshotPath: configImageComparison && configImageComparison.hasOwnProperty('screenshotPath') ?
+                configImageComparison.screenshotPath : './images/given/',
+        });
 
-/**
- * function to open an url path to given domain
- * @param urlPath
- */
-function get(path) {
-    return browser.get(browser.baseUrl + path);
-}
-
-/**
- * extended selenium element.isDisplayed function
- * @param element
- * @param options
- * @returns {promise.Promise<boolean> | webdriver.promise.Promise.<boolean>}
- */
-function isDisplayed(element, options) {
-    options = optionSetter(options);
-    optionExecutor(element, options);
-    return element.isDisplayed();
-}
-
-/**
- * extended selenium element.getText function
- * @param element
- * @param options
- * @returns {promise.Promise<boolean> | webdriver.promise.Promise.<boolean>}
- */
-function getText(element, options) {
-    options = optionSetter(options);
-    optionExecutor(element, options);
-    return element.getText();
-}
-
-/**
- * function to clear cookies, session, storage
- */
-function clearBrowserInstance() {
-    browser.manage().deleteAllCookies();
-    browser.executeScript('window.localStorage.clear()');
-    return browser.executeScript('sessionStorage.clear()');
-}
-
-/**
- * function to wait until an element is clickable
- * @param element
- * @param options
- */
-function waitClickable(element, options) {
-    options = optionSetter(options);
-    return browser.wait(protractor.ExpectedConditions.elementToBeClickable(element), options.waitClickable).then().catch( () => {});
-}
-
-/**
- * function to scroll element into viewport
- * @param element
- */
-function scrollIntoView(element) {
-    return browser.executeScript('arguments[0].scrollIntoView();', element.getWebElement());
-}
-
-/**
- * extended image comparison function check screen
- * @param tag
- * @param options
- * @returns {Promise}
- */
-function checkScreen(tag, options) {
-    options = optionSetter(options);
-    optionExecutor(element(by.css('html')), options);
-    return protractorImageComparison.checkScreen(tag, options.imageComparison);
-}
-
-/**
- * extended image comparison function check element
- * @param tag
- * @param element
- * @param options
- * @returns {Promise}
- */
-function checkElement(tag, element, options) {
-    options = optionSetter(options);
-    optionExecutor(element, options);
-    return protractorImageComparison.checkElement(element, tag, options.imageComparison);
-}
-
-/**
- * Check if element has a given class
- * @param element
- * @param cls - class name
- * @return {promise.Promise<boolean>}
- */
-function hasClass(element, cls) {
-    return element.getAttribute('class').then(function(classes) {
-        return classes.split(' ').indexOf(cls) !== -1;
-    });
-}
-
-/**
- * switch to specific browser tab
- * @param tab
- */
-function switchToTab(tab) {
-    return browser.getAllWindowHandles().then(function(handles) {
-        return browser.switchTo().window(handles[tab]);
-    });
-}
-
-/**
- * private function to set options {wait, waitClickable, scrollIntoView, moveMouse, listElement, imageComparison}
- * @param options
- * @returns {{}}
- */
-function optionSetter(options) {
-    return {
-        wait: options && options.hasOwnProperty('wait') ? options.wait : 500,
-        waitClickable: options && options.hasOwnProperty('waitClickable') ? options.waitClickable : 10000,
-        scrollIntoView: options && options.hasOwnProperty('scrollIntoView') ? options.scrollIntoView : false,
-        moveMouse: options && options.hasOwnProperty('moveMouse') ? options.moveMouse : false,
-        imageComparison: options && options.hasOwnProperty('imageComparison') ? options.imageComparison : {},
     };
+
+    /**
+     * function to open an url path to given domain
+     * @param path
+     */
+    get(path) {
+        return browser.get(browser.baseUrl + path);
+    }
+
+    /**
+     * extended selenium element.click function
+     * @param element
+     * @param options
+     * @returns {ActionSequence | promise.Promise<void> | webdriver.promise.Promise.<void> | *}
+     */
+    click(element, options) {
+        options = this._optionSetter(options);
+        this._optionExecutor(element, options);
+        return element.click();
+    }
+
+    /**
+     * function to select an element of a dropdown list
+     * @param element
+     * @param elementOption
+     * @param elementOptionNumber
+     * @param options
+     * @returns {ActionSequence|promise.Promise.<void>|webdriver.promise.Promise.<void>|*}
+     */
+    clickDropdown(element, elementOption, elementOptionNumber, options) {
+        options = this._optionSetter(options);
+        this.click(element, options);
+        return this.click(element.all(elementOption).get(elementOptionNumber), options);
+    }
+
+     /**
+     * extended selenium element.sendKeys function
+     * @param element
+     * @param text
+     * @param options
+     * @returns {promise.Promise<any>}
+     */
+    sendKeys(element, text, options) {
+        options = this._optionSetter(options);
+        this._optionExecutor(element, options);
+        return element.clear().then(function() {
+            return element.sendKeys(text);
+        });
+    }
+
+    /**
+     * extended selenium element.getText function
+     * @param element
+     * @param options
+     * @returns {promise.Promise<string> | webdriver.promise.Promise.<string> | promise.Promise.<boolean> | webdriver.promise.Promise.<boolean>}
+     */
+    getText(element, options) {
+        options = this._optionSetter(options);
+        this._optionExecutor(element, options);
+        return element.getText();
+    }
+
+    /**
+     * extended selenium element.isDisplayed function
+     * @param element
+     * @param options
+     * @returns {webdriver.promise.Promise.<boolean>|promise.Promise<boolean>|*}
+     */
+    isDisplayed(element, options) {
+        options = this._optionSetter(options);
+        this._optionExecutor(element, options);
+        return element.isDisplayed();
+    }
+
+    /**
+     * Check if element has a given class
+     * @param element
+     * @param cls
+     * @returns {promise.Promise<any>}
+     */
+    hasClass(element, cls) {
+        return element.getAttribute('class').then(function(classes) {
+            return classes.split(' ').indexOf(cls) !== -1;
+        });
+    }
+
+    /**
+     * function to wait until an element is clickable
+     * @param element
+     * @param options
+     * @returns {boolean}
+     */
+    waitClickable(element, options) {
+        options = this._optionSetter(options);
+        return browser.wait(protractor.ExpectedConditions.elementToBeClickable(element), options.waitClickable).then().catch( () => {});
+    }
+
+    /**
+     * function to scroll element into viewport
+     * @param element
+     * @returns {promise.Promise.<T>|promise.Promise<any>}
+     */
+    scrollIntoView(element) {
+        return browser.executeScript('arguments[0].scrollIntoView();', element.getWebElement());
+    }
+
+    /**
+     * function to clear cookies, session, storage
+     * @returns {promise.Promise.<T>|promise.Promise<any>}
+     */
+    clearBrowserInstance() {
+        browser.manage().deleteAllCookies();
+        browser.executeScript('window.localStorage.clear()');
+        return browser.executeScript('sessionStorage.clear()');
+    }
+
+    /**
+     * extended image comparison function check screen
+     * @param tag
+     * @param options
+     * @return {Promise}
+     */
+    checkScreen(tag, options) {
+        options = this._optionSetter(options);
+        this._optionExecutor(element(by.css('html')), options);
+        return this.protractorImageComparison.checkScreen(tag, options.imageComparison);
+    }
+
+    /**
+     * extended image comparison function check element
+     * @param tag
+     * @param element
+     * @param options
+     * @return {Promise}
+     */
+    checkElement(tag, element, options) {
+        options = this._optionSetter(options);
+        this._optionExecutor(element, options);
+        return this.protractorImageComparison.checkElement(element, tag, options.imageComparison);
+    }
+
+    /**
+     * switch to specific browser tab
+     * @param tab
+     * @returns {promise.Promise<any>}
+     */
+    switchToTab(tab) {
+        return browser.getAllWindowHandles().then(function(handles) {
+            return browser.switchTo().window(handles[tab]);
+        });
+    }
+
+    /**
+     * private function to set options {wait, waitClickable, scrollIntoView, moveMouse, listElement, imageComparison}
+     * @param options
+     * @returns {{wait: *, waitClickable: number, scrollIntoView: boolean, moveMouse: boolean, imageComparison: {}}}
+     * @private
+     */
+    _optionSetter(options) {
+        return {
+            wait: options && options.hasOwnProperty('wait') ? options.wait : this.config.wait,
+            waitClickable: options && options.hasOwnProperty('waitClickable') ? options.waitClickable : this.config.waitClickable,
+            scrollIntoView: options && options.hasOwnProperty('scrollIntoView') ? options.scrollIntoView : this.config.scrollIntoView,
+            imageComparison: options && options.hasOwnProperty('imageComparison') ? options.imageComparison : this.config.imageComparison,
+        };
+    }
+
+    /**
+     * private function to execute options
+     * @param element
+     * @param options
+     * @private
+     */
+    _optionExecutor(element, options) {
+        if (options.wait !== false && options.wait !== 0) {
+            browser.sleep(options.wait);
+        }
+        if (options.scrollIntoView) {
+            this.scrollIntoView(element);
+        }
+        if (options.waitClickable !== false && options.waitClickable !== 0) {
+            this.waitClickable(element, options);
+        }
+    }
+
 }
 
-/**
- * private function to execute options
- * @param options
- */
-function optionExecutor(element, options) {
-    if (options.wait !== false && options.wait !== 0) {
-        browser.sleep(options.wait);
-    }
-    if (options.scrollIntoView) {
-        scrollIntoView(element);
-    }
-    if (options.moveMouse === true) {
-        browser.actions().mouseMove(element).perform();
-    }
-    if (options.waitClickable !== false && options.waitClickable !== 0) {
-        waitClickable(element, options);
-    }
-}
-
-module.exports = {
-    click: click,
-    clickDropdown: clickDropdown,
-    sendKeys: sendKeys,
-    getText: getText,
-    get: get,
-    isDisplayed: isDisplayed,
-    clearBrowserInstance: clearBrowserInstance,
-    waitClickable: waitClickable,
-    scrollIntoView: scrollIntoView,
-    checkScreen: checkScreen,
-    checkElement: checkElement,
-    hasClass: hasClass,
-    switchToTab: switchToTab,
-};
+module.exports = ProtractorUtilsModule;
